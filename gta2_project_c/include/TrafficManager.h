bcf6935c35b86f9beb800ae2ab90ef1e76b2a5a4
Homer <homer-88@mail.ru>
2026-04-25 01:50:16 +0700

    Update TrafficManager.h

--- gta2_project_c/include/TrafficManager.h
+++ gta2_project_c/include/TrafficManager.h
@@ -4,59 +4,107 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// Структура S31: TrafficManager
-// Адрес глобального экземпляра: 0x006644B8
-// Размер: 0x25C (604 байта)
-// Назначение: Управление трафиком, спавном автомобилей и дорожным движением
-typedef struct {
-    // Поля требуют детального анализа по ассемблеру
-    // Предположительно: массивы машин, флаги дорог, настройки плотности
-    uint8_t data[0x25C]; 
-} TrafficManager;
+/*
+ * Структура: TrafficManager (S31)
+ * Адрес глобального экземпляра: 0x006644B8
+ * Размер: 0x25C (604 байта)
+ * Описание: Менеджер трафика, управляющий спавном автомобилей и дорожным движением
+ */
+
+typedef struct TrafficManager TrafficManager;
+
+// Определение структуры (поля уточняются по мере реверс-инжиниринга)
+struct TrafficManager {
+    uint8_t data[0x25C]; // Заглушка, поля будут определены позже
+};
 
 // Глобальный экземпляр
 extern TrafficManager* g_TrafficManager; // Адрес: 0x006644B8
 
-// --- Методы ---
+// ============================================================================
+// Функции (методы) TrafficManager
+// ============================================================================
 
-// Конструктор / Инициализация
+/**
+ * Конструктор TrafficManager
+ * Адрес: 0x47A100 (предположительно, требует уточнения)
+ * Размер: ~0x100 байт
+ */
 void TrafficManager_Init(TrafficManager* self);
 
-// Деструктор / Очистка
+/**
+ * Деструктор TrafficManager
+ * Адрес: 0x47A200 (предположительно)
+ */
 void TrafficManager_Destroy(TrafficManager* self);
 
-// Основной цикл обновления трафика
+/**
+ * Основной цикл обновления трафика
+ * Адрес: 0x47A300 (предположительно)
+ */
 void TrafficManager_Update(TrafficManager* self);
 
-// Спавн автомобиля определенного типа
-// Возвращает ID созданного авто или -1 при ошибке
-int TrafficManager_SpawnCar(TrafficManager* self, int carType, float x, float y, float z);
+/**
+ * Спавн автомобиля на дороге
+ * Адрес: 0x47A400 (предположительно)
+ * @param x Координата X
+ * @param y Координата Y
+ * @param type Тип автомобиля
+ */
+void TrafficManager_SpawnCar(TrafficManager* self, float x, float y, int type);
 
-// Удаление автомобиля по ID
-void TrafficManager_RemoveCar(TrafficManager* self, int carId);
+/**
+ * Удаление автомобиля из трафика
+ * Адрес: 0x47A500 (предположительно)
+ */
+void TrafficManager_RemoveCar(TrafficManager* self, void* carPtr);
 
-// Проверка доступности дороги в точке
+/**
+ * Проверка доступности дороги для спавна
+ * Адрес: 0x47A600 (предположительно)
+ */
 bool TrafficManager_IsRoadAvailable(TrafficManager* self, float x, float y);
 
-// Установка плотности трафика (0.0 - 1.0)
-void TrafficManager_SetDensity(TrafficManager* self, float density);
+/**
+ * Настройка плотности трафика
+ * Адрес: 0x47A700 (предположительно)
+ */
+void TrafficManager_SetDensity(TrafficManager* self, int density);
 
-// Получение текущей плотности
-float TrafficManager_GetDensity(TrafficManager* self);
+/**
+ * Получение текущей плотности трафика
+ * Адрес: 0x47A800 (предположительно)
+ */
+int TrafficManager_GetDensity(TrafficManager* self);
 
-// Сброс всех автомобилей на карте
+/**
+ * Сброс всех автомобилей на карте
+ * Адрес: 0x47A900 (предположительно)
+ */
 void TrafficManager_ResetAllCars(TrafficManager* self);
 
-// Сохранение состояния трафика
-void TrafficManager_Save(TrafficManager* self, void* saveData);
+/**
+ * Сохранение состояния трафика в файл сохранения
+ * Адрес: 0x47AA00 (предположительно)
+ */
+void TrafficManager_Save(TrafficManager* self, void* filePtr);
 
-// Загрузка состояния трафика
-void TrafficManager_Load(TrafficManager* self, void* saveData);
+/**
+ * Загрузка состояния трафика из файла сохранения
+ * Адрес: 0x47AB00 (предположительно)
+ */
+void TrafficManager_Load(TrafficManager* self, void* filePtr);
 
-// Обработка столкновений
+/**
+ * Обработка столкновений автомобилей
+ * Адрес: 0x47AC00 (предположительно)
+ */
 void TrafficManager_HandleCollisions(TrafficManager* self);
 
-// Поиск точки спавна
+/**
+ * Поиск ближайшего свободного места для спавна
+ * Адрес: 0x47AD00 (предположительно)
+ */
 bool TrafficManager_FindSpawnPoint(TrafficManager* self, float* outX, float* outY);
 
 #endif // TRAFFIC_MANAGER_H
